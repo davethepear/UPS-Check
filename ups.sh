908f9dd3971bcf5f4e96e17f16c246fb28aedb4d
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-11-09 17:11:05 -0600

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -5,24 +5,25 @@ user=servers
 
 nc -z $ups $port > /dev/null
 if [ "$?" = "1" ]; then
-echo "are you sure you have the right address and port? maybe the user is wrong?"
+    echo "are you sure you have the right address and port? maybe the user is wrong?"
     exit 1
 fi
 timestamp=$( date +%T )
-status=$( upsc $user@$ups:$port 2>&1 | grep -v '^Init SSL' | grep ups.status: | awk '{ print $2 }' )
-if echo "$status" | grep 'OL' ; then
+status=$( upsc $user@$ups:$port 2>&1 ups.status | grep -v '^Init SSL' )
+if [ "$status" = "OL" ]; then
    echo Power is on... yay!
    exit 1
 else
-# Typical of me, I'm not sure about having it shutdown, so it's echoed. I've been having "data stale" problems.
+# Typical of me, I am not sure about having it shutdown, so it is echoed. I've been having data stale problems.
    echo "shutdown -P" | mail -s "Is the power out?!" ubuntu
 fi
 # if the power is out... ask for percentaget of battery
-batt=$( upsc $user@$ups:$port 2>&1 | grep -v '^Init SSL' | grep battery.charge: | awk '{ print $2 }' )
-echo "$batt"
+batt=$( upsc $user@$ups:$port battery.charge 2>&1 | grep -v '^Init SSL' )
 if [ "$batt" -gt "95" ]; then
+    echo "$batt"
     exit 1
 else
 # Again, the shutdown it echoed, but it emails me. Luckily the power's stayed on and I'm too lazy to unplug it.
     echo "shutdown -P" | mail -s "Is the power out? There is $batt percent left!" ubuntu
 fi
+
