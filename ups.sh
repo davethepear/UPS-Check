c01b6e5202a71bbab30cb2771f0305520178f549
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-11-09 17:22:36 -0600

    Update ups.sh
    added -v so chron wouldn't email me constantly

--- ups.sh
+++ ups.sh
@@ -11,7 +11,8 @@ fi
 timestamp=$( date +%T )
 status=$( upsc $user@$ups:$port 2>&1 ups.status | grep -v '^Init SSL' )
 if [ "$status" = "OL" ]; then
-   echo Power is on... yay!
+   if [ "$1" = "-v" ]; then echo Power is on... yay!
+   fi
    exit 1
 else
 # Typical of me, I am not sure about having it shutdown, so it is echoed. I've been having data stale problems.
@@ -19,11 +20,12 @@ else
 fi
 # if the power is out... ask for percentaget of battery
 batt=$( upsc $user@$ups:$port battery.charge 2>&1 | grep -v '^Init SSL' )
+# I have the batt level set pretty high, just to get a response from it. if it works, I'll set it lower.
 if [ "$batt" -gt "95" ]; then
-    echo "$batt"
+   if [ "$1" = "-v" ]; then  echo "$batt"
+   fi
     exit 1
 else
 # Again, the shutdown it echoed, but it emails me. Luckily the power's stayed on and I'm too lazy to unplug it.
     echo "shutdown -P" | mail -s "Is the power out? There is $batt percent left!" ubuntu
 fi
-
